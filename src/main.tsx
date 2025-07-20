4fab1966a96bd82f67f3fbfd21fcb45457d687e5
huyq84 <61939036+huyq84@users.noreply.github.com>
2025-07-20 18:34:07 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,13 @@
 
+// src/main.jsx (或 main.tsx)
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // <-- 这里导入了 index.css
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
